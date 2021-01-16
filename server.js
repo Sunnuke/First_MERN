6311503419b797ec04b8a0deedd7f906ecbd3d58
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-16 14:32:59 -0800

    Server Test run was functional and displayed the JSON object; message: HELLO WORLDgit add .! , in postman

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+
 require("./server/routes/person.routes")(app);
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
