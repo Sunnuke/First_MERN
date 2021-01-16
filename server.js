c3454175b2948e26bf41ab2b125ac80dbb131a63
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-16 15:03:56 -0800

    Imported cors into server.js, cors installs the ability to make cross-origin requests between react and nodemon servers

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
-
-
+app.use(cors());
 require("./server/routes/person.routes")(app);
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
