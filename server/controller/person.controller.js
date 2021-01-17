df585764535c4303be2fa88f6b9f89defe4aa1b2
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-17 11:55:27 -0800

    Updated: person.controller.js, added a createPerson module

--- server/controller/person.controller.js
+++ server/controller/person.controller.js
@@ -1,8 +1,19 @@
 const { request, response } = require("express");
 const { model, models } = require("mongoose");
+const { Person } = require("../models/person.model");
 
 module.exports.index = (request, response) => {
     response.json({
         message: "HELLO WORLD!!!"
     });
+}
+
+module.exports.createPerson = (request, response) => {
+    const { firstName, lastName } = request.body;
+    Person.create({
+        firstName,
+        lastName
+    })
+    .then(person => response.json(person))
+    .catch(error => response.json(error));
 }
