ed0ceb3e82c02d2ed7db880d999daeb59fdcc54f
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-17 19:10:03 -0800

    Updated: Main.js, to display PersonForm component

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
+import PersonForm from "../components/PersonForm";
 
 export default () => {
-    const [ message, setMessage ] = useState("Loading...");
-
-    useEffect(() => {
-        axios.get("http://localhost:8000/api")
-            .then(res => setMessage(res.data.message))
-    }, []);
     return(
-        <div>
-            <h1>Message from Backend: {message}</h1>
-        </div>
+        <PersonForm />
     )
 }
