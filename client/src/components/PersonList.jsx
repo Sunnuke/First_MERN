902f8439964dcb6c2e089cfdd7aca42d0d53941b
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-19 12:38:53 -0800

    Update: Added a remove/deletePerson function to Main.js and PersonList.jsx

--- client/src/components/PersonList.jsx
+++ client/src/components/PersonList.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import axios from 'axios';
+import { Link } from '@reach/router';
 export default props => {
+    const { removeFromDom } = props;
+    const deletePerson = (personId) => {
+        axios.delete('http://localhost:8000/api/person/' + personId)
+            .then(res => {
+                removeFromDom(personId)
+            })
+    }
     return (
         <div>
-            {props.people.map((person, idx)=>{
-                return <p key={idx}>{person.lastName}, {person.firstName}</p>
+            {props.people.map((person, idx) => {
+                return <p key={idx}>
+                    <Link to={"/" + person._id}>
+                        {person.lastName}, {person.firstName}
+                    </Link>
+                    |
+                    <button onClick={(e)=>{deletePerson(person._id)}}>
+                        Delete
+                    </button>
+                </p>
             })}
         </div>
     )
