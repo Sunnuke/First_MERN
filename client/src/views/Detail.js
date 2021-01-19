0f1f7b01e4c263b8d3556e0e128e8d94320fdec8
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 16:32:01 -0800

    Setup Detail.js, to display a selected person object from the database

---
+++ client/src/views/Detail.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+export default props => {
+    const [person, setPerson] = useState({})
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/person/" + props.id)
+            .then(res => setPerson(res.data))
+    }, [])
+    return (
+        <div>
+            <p>First Name: {person.firstName}</p>
+            <p>Last Name: {person.lastName}</p>
+        </div>
+    )
+}
