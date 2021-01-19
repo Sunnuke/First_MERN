104b925e5f7aa37d1ea174a1e1329cb4b14ad464
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 16:27:13 -0800

    Update: imported PersonList into Main, and added an api call

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from "react";
-import PersonForm from "../components/PersonForm";
-
+import React, { useEffect, useState } from 'react'
+import PersonForm from '../components/PersonForm';
+import PersonList from '../components/PersonList';
 export default () => {
-    return(
-        <PersonForm />
+    const [people, setPeople] = useState([]);
+    const [loaded, setLoaded] = useState(false);
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/person')
+            .then(res=>{
+                setPeople(res.data);
+                setLoaded(true);
+            });
+    },[])
+    return (
+        <div>
+            <PersonForm/>
+            <hr/>
+            {loaded && <PersonList people={people}/>}
+        </div>
     )
 }
