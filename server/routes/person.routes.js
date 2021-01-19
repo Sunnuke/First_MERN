bb20abc5b8689acb5e2b703821a488cefe8c2e88
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-19 11:44:33 -0800

    Update: Added an updateOne and deleteOne route to person.routes.js

--- server/routes/person.routes.js
+++ server/routes/person.routes.js
@@ -2,7 +2,13 @@ const PersonController = require("../controller/person.controller");
 
 module.exports = (app) => {
     app.get("/api", PersonController.index);
+
     app.post("/api/person/new", PersonController.createPerson);
+
     app.get('/api/person', PersonController.getAllPeople);
     app.get('/api/person/:id', PersonController.getPerson);
+
+    app.put('/api/people/:id', PersonController.updatePerson);
+
+    app.delete('/api/people/:id', PersonController.deletePerson);
 }
