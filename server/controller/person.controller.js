38256f039d1740972e93718a25d69ed6ced73e81
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 16:12:38 -0800

    Update: Added a findAll and findOne module to person.controller.js

--- server/controller/person.controller.js
+++ server/controller/person.controller.js
@@ -17,3 +17,15 @@ module.exports.createPerson = (request, response) => {
         .then(person => response.json(person))
         .catch(error => response.json(error));
 }
+
+module.exports.getAllPeople = (request, response) => {
+    Person.find({})
+        .then(persons => response.json(persons))
+        .catch(err => response.json(err))
+}
+
+module.exports.getPerson = (request, response) => {
+    Person.findOne({_id:request.params.id})
+        .then(person => response.json(person))
+        .catch(err => response.json(err))
+}
