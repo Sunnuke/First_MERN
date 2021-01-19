b2da202cd0df7071a667b08d6105e522d7fd0f93
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-18 16:35:07 -0800

    Update: imported Router and Detail.js into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import Main from "./views/Main";
-import './App.css';
-
+import React from 'react';
+import { Router } from '@reach/router';
+import Main from './views/Main';
+import Detail from './views/Detail';
 function App() {
   return (
     <div className="App">
-      <Main />
+      <Router>
+        <Main path="people/"/>
+        <Detail path="people/:id" />
+      </Router>
     </div>
   );
 }
-
 export default App;
